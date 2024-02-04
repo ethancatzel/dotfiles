c95a8e12ddd44f2fae1a4211e20775eae67f18fb
Ethan Catzel <ethancatzel@gmail.com>
2024-02-05 10:55:44 +1100

    update tf conf to 2 spaces

--- nvim/lua/ethan/init.lua
+++ nvim/lua/ethan/init.lua
@@ -1,3 +1,11 @@
 require("ethan.set")
 require("ethan.remap")
 require("ethan.lazy_init")
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "terraform",
+  callback = function()
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+  end,
+})
