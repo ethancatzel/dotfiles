ba418f924cbfd0616548986b36098addd600f04b
Ethan Catzel <ethancatzel@gmail.com>
2024-02-08 17:09:50 +1100

    add lsp goto-definition

--- nvim/lua/ethan/init.lua
+++ nvim/lua/ethan/init.lua
@@ -3,9 +3,16 @@ require("ethan.remap")
 require("ethan.lazy_init")
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "terraform",
-  callback = function()
-    vim.opt_local.shiftwidth = 2
-    vim.opt_local.tabstop = 2
-  end,
+    pattern = "terraform",
+    callback = function()
+        vim.opt_local.shiftwidth = 2
+        vim.opt_local.tabstop = 2
+    end,
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(e)
+        local opts = { buffer = e.buf }
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    end
 })
