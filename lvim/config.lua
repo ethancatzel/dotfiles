661b50ebcdecef58eb00adbbfcd8642115011975
Ethan Catzel <ethancatzel@gmail.com>
2024-02-12 16:04:29 +1100

    harpoon

--- lvim/config.lua
+++ lvim/config.lua
@@ -4,5 +4,23 @@
 -- Discord: https://discord.com/invite/Xb9B4Ny
 
 vim.opt.relativenumber = true
+vim.opt.wrap = false
 
 lvim.builtin.bufferline.active = false -- Remove tab lines.
+lvim.format_on_save.enabled = true
+
+lvim.plugins = {
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local harpoon = require("harpoon")
+
+      harpoon:setup()
+
+      vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+      vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+    end
+  }
+}
