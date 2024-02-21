e94127bbfa07b14bad17333b266e62319794f32e
Ethan Catzel <ethancatzel@gmail.com>
2024-02-21 20:47:48 +1100

    Add copilot

--- lvim/config.lua
+++ lvim/config.lua
@@ -12,6 +12,7 @@ lvim.format_on_save.enabled = true
 lvim.keys.normal_mode["<C-d>"] = "<C-d>zz"
 lvim.keys.normal_mode["<C-u>"] = "<C-u>zz"
 lvim.keys.normal_mode["<C-.>"] = "<cmd>lua vim.lsp.buf.code_action()<CR>"
+lvim.keys.normal_mode["<leader>s"] = "<cmd>lua vim.diagnostic.open_float()<CR>"
 
 lvim.plugins = {
   {
@@ -26,5 +27,39 @@ lvim.plugins = {
       vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
       vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
     end
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({})
+    end,
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    config = function()
+      require("copilot_cmp").setup({
+        suggestion = { enabled = false },
+        panel = { enabled = false }
+      })
+    end
   }
 }
+
+-- Below config is required to prevent copilot overriding Tab with a suggestion
+-- when you're just trying to indent!
+local has_words_before = function()
+  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+end
+local on_tab = vim.schedule_wrap(function(fallback)
+  local cmp = require("cmp")
+  if cmp.visible() and has_words_before() then
+    cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+  else
+    fallback()
+  end
+end)
+lvim.builtin.cmp.mapping["<Tab>"] = on_tab
