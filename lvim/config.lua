f9c99b208a5cf989a73031b6060cdf4fa42531e8
Ethan Catzel <ethancatzel@gmail.com>
2024-04-11 18:47:08 +1000

    remove copilot and add prettier fmter

--- lvim/config.lua
+++ lvim/config.lua
@@ -16,7 +16,8 @@ lvim.keys.normal_mode["<leader>s"] = "<cmd>lua vim.diagnostic.open_float()<CR>"
 
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
-  { name = "black" }
+  { name = "black" },
+  { name = "prettier" }
 }
 
 lvim.plugins = {
@@ -34,14 +35,6 @@ lvim.plugins = {
     end
   },
   {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-      require("copilot").setup({})
-    end,
-  },
-  {
     "zbirenbaum/copilot-cmp",
     config = function()
       require("copilot_cmp").setup({
@@ -51,20 +44,3 @@ lvim.plugins = {
     end
   }
 }
-
--- Below config is required to prevent copilot overriding Tab with a suggestion
--- when you're just trying to indent!
-local has_words_before = function()
-  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
-end
-local on_tab = vim.schedule_wrap(function(fallback)
-  local cmp = require("cmp")
-  if cmp.visible() and has_words_before() then
-    cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-  else
-    fallback()
-  end
-end)
-lvim.builtin.cmp.mapping["<Tab>"] = on_tab
